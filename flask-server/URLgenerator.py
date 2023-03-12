6292c91e205e55e586c0491e0fc096fe04d2ed49
Kimberly Ruano <kimberlyruano@Kimberlys-MBP.attlocal.net>
2023-03-11 23:03:05 -0600

    Updated URL generator

--- flask-server/URLgenerator.py
+++ flask-server/URLgenerator.py
@@ -1,28 +1,27 @@
 
-
 import string
 import random
 
+
+def generate_short_url(longURL: str):
+    alphabet = list(string.ascii_letters)
+    counter = 0
+    shortURL = ""
+
+    while counter < 6:
+        shortURL = shortURL + random.choice(alphabet)
+        counter = counter + 1
+    
+    print(shortURL)
+
+generate_short_url("www.google.com")
+
+
+
+
+
+#Notes:
 # alphabetLower = list(string.ascii_lowercase)
 # alphabetUpper = list(string.ascii_uppercase)
 # print(alphabetLower)
 # print(alphabetUpper)
-
-alphabet = list(string.ascii_letters)
-
-def get_random_string(length):
-    result_str = ''.join(random.choice(alphabet) for i in range(length))
-    print("Random string of length", length, "is:", result_str)
-
-get_random_string(6)
-
-
-
-
-# >>> def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
-# ...    return ''.join(random.choice(chars) for _ in range(size))
-# ...
-# >>> id_generator()
-# 'G5G74W'
-# >>> id_generator(3, "6793YUIO")
-# 'Y3U'
