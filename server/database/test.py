f1e74b253a753642d013b10acd8fc1f0d6d3400e
Kimberly Ruano <kimberlyruano@Kimberlys-MBP.attlocal.net>
2023-03-15 16:02:59 -0500

    Created short_url_exists

--- server/database/test.py
+++ server/database/test.py
@@ -1,10 +1,12 @@
 # insert and query the rows
 
-from db_api import DbApi #From db_api file import the DbApi class
+from db_api import DbApi  # From db_api file import the DbApi class
 
-#Context manager: Creates the object, executes the function called, calles exit function and closes the object
-with DbApi() as db: #db = DbApi() 
+# Context manager: Creates the object, executes the function called, calles exit function and closes the object
+with DbApi() as db:  # db = DbApi()
     # db.query_tables()
     # db.query_all_URL()
-    db.insert_URL(longURL="www.fb.com", shortURL="eRaEnp")
-    db.query_all_URL()
+    # db.insert_URL(longURL="www.fb.com", shortURL="eRaEnp")
+    db.query_all_url()
+    # db.long_url_exists("www.google.com")
+    db.short_url_exists("DefSwe")
